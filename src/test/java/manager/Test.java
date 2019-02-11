b489d05fb1f7b333473efff1dee754febc2b8dcd
user <user@user-PC>
2019-02-11 10:42:46 +0800

    初始化

--- src/test/java/manager/Test.java
+++ src/test/java/manager/Test.java
@@ -1,10 +1,26 @@
 package manager;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Test {
+	
+	public static int test(String key) {
+		int h;
+		return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+		HashMap<String, String> map = new HashMap<>();
+		map.put("1", "1");
+		Scanner scanner = new Scanner(System.in);
+		while(scanner.hasNext()) {
+			String string = scanner.nextLine();
+			System.out.println(test(string));
+			
+		}
+		
 	}
 
 }
