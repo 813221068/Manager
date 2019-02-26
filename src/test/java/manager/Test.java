6a3f0352a3e3eab8433ae746d3050790114f473a
user <user@user-PC>
2019-02-26 17:43:23 +0800

    添加审批流程

--- src/test/java/manager/Test.java
+++ src/test/java/manager/Test.java
@@ -1,18 +1,18 @@
 package manager;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-
 public class Test {
 	
 	public static int test(String key) {
 		int h;
 		return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
 	}
-	
+
 	public static void main(String[] args) {
-	System.out.println("123"+"\r\n123");
 		
 	}
 
